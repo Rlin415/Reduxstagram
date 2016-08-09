c64bbf447bfa867c8c54fd40cd1eea3d2718634b
Robert Lin <Rlin415@gmail.com>
2016-08-09 14:17:07 -0700

    create redux reducers

---
+++ client/reducers/comments.js
@@ -0,0 +1,7 @@
+
+export default comments;
+
+function comments(state = [], action) {
+  console.log(state, action);
+  return state;
+}
---
+++ client/reducers/posts.js
@@ -0,0 +1,7 @@
+
+export default posts;
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
