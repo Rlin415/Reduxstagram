a059fa8be4d21482745fe3fddc360c2612c0c2d4
Robert Lin <Rlin415@gmail.com>
2016-08-16 23:04:20 -0700

    update state with reducers

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -2,6 +2,5 @@
 export default comments;
 
 function comments(state = [], action) {
-  console.log(state, action);
   return state;
 }
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -2,7 +2,17 @@
 export default posts;
 
 function posts(state = [], action) {
-  console.log('post will change');
-  console.log(state, action);
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('Incrementing Likes');
+      const i = action.index;
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1)
+      ]
+    default:
+      return state;
+  }
   return state;
 }
